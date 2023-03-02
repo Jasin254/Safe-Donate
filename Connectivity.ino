de34bdaa85fcb93b29581d5ad3e47845e687c9a0
Jasin Owili <51453674+Jasin254@users.noreply.github.com>
2023-03-02 21:59:37 +0300

    share data from the wearable watch- BT and WiFi
    In this code, the Bluetooth communication is established using the SoftwareSerial library, and the vital signal and battery data are sent to the mobile app via Bluetooth using the btSerial.print() and btSerial.println() functions.
    
    The WiFi communication is established using the WiFi library, and the vital signal and battery data are sent to a server using HTTP POST requests. The client.connect() function is used to establish a connection to the server, and the data is sent using the client.println() function.

---
+++ Connectivity.ino
@@ -0,0 +1,65 @@
+#include <SoftwareSerial.h>
+#include <WiFi.h>
+
+const char* ssid = "your_wifi_ssid";
+const char* password = "your_wifi_password";
+const char* host = "your_server_address";
+const int port = 80;
+
+SoftwareSerial btSerial(2, 3); // RX, TX for Bluetooth module
+WiFiClient client;
+
+void setup() {
+  Serial.begin(9600);
+  btSerial.begin(9600);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  int heartRate = getHeartRate();
+  int bloodOxygen = getBloodOxygen();
+  float batteryVoltage = getBatteryVoltage();
+  
+  // Send data to mobile app via Bluetooth
+  btSerial.print("HR:");
+  btSerial.print(heartRate);
+  btSerial.print(" O2:");
+  btSerial.print(bloodOxygen);
+  btSerial.print(" BV:");
+  btSerial.println(batteryVoltage);
+
+  // Send data to server via WiFi
+  if (client.connect(host, port)) {
+    String data = "HR=" + String(heartRate) + "&O2=" + String(bloodOxygen) + "&BV=" + String(batteryVoltage);
+    client.println("POST /data HTTP/1.1");
+    client.println("Host: " + String(host));
+    client.println("Connection: close");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.println(data.length());
+    client.println();
+    client.println(data);
+    client.println();
+    Serial.println("Data sent to server");
+  } else {
+    Serial.println("Connection to server failed");
+  }
+  delay(5000); // wait for 5 seconds before sending data again
+}
+
+int getHeartRate() {
+  // get heart rate from sensor
+}
+
+int getBloodOxygen() {
+  // get blood oxygen from sensor
+}
+
+float getBatteryVoltage() {
+  // get battery voltage from ADC
+}
