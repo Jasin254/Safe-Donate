c79eb53f0886b59b709447e7bebaa8c260d34ce2
Jasin Owili <51453674+Jasin254@users.noreply.github.com>
2023-03-02 21:22:47 +0300

    Max80100 Firmware
    This code is written in Arduino IDE and uses the MAX30105 sensor to measure the heart rate and blood oxygen levels. It also uses an SSD1306 OLED display to display the measured values. The code continuously checks for a heartbeat and calculates the heart rate in beats per minute. It then displays the heart rate and blood oxygen levels on the OLED display.

---
+++ Max30105.ino
@@ -0,0 +1,63 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <MAX30105.h>
+#include <heartRate.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4;
+byte rates[RATE_SIZE];
+byte rateSpot = 0;
+long lastBeat = 0;
+float beatsPerMinute;
+int beatAvg;
+
+void setup() {
+  Serial.begin(9600);
+  particleSensor.begin(Wire, I2C_SPEED_FAST);
+  particleSensor.setup();
+  particleSensor.setPulseAmplitudeRed(0x0A);
+  particleSensor.setPulseAmplitudeGreen(0);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.println("Vital Sign Monitor");
+  display.display();
+  delay(1000);
+}
+
+void loop() {
+  long irValue = particleSensor.getIR();
+  if (checkForBeat(irValue) == true) {
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+    beatsPerMinute = 60 / (delta / 1000.0);
+    if (beatsPerMinute < 255 && beatsPerMinute > 20) {
+      rates[rateSpot++] = (byte)beatsPerMinute;
+      rateSpot %= RATE_SIZE;
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+  }
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.println("Heart Rate:");
+  display.setTextSize(2);
+  display.setCursor(0,20);
+  display.println(beatAvg);
+  display.setTextSize(1);
+  display.setCursor(0,45);
+  display.println("Blood Oxygen:");
+  display.setCursor(0,55);
+  display.println(particleSensor.getFIFO() & 0x3FF);
+  display.display();
+  delay(100);
+}
